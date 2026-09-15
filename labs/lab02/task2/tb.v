e26b44f46f2fd82bf998785394b1e2493925b97e
AryanOmar3-AO <f20240832@pilani.bits-pilani.ac.in>
2026-09-15 16:29:12 +0000

    lab02 task2

--- labs/lab02/task2/tb.v
+++ labs/lab02/task2/tb.v
@@ -4,9 +4,16 @@
 module tb;
 
   // TODO: declare the inputs and outputs
-
+  reg [2:0] t_sel;
+  wire [7:0] t_dout;
   // TODO: instantiate DUT here
-
+  lut #(
+    .WIDTH(8),
+    .DEPTH(8)
+  ) DUT (
+    .sel(t_sel),
+    .dout(t_dout)
+  );
   // Waveform dump configuration (DO NOT CHANGE)
   string vcd_file;
   initial begin
@@ -17,11 +24,42 @@ module tb;
   end
 
   initial begin
-    // TODO: apply different input combinations
+     t_sel = 3'd0;
+    #5;
 
+    // SEL = 1
+    t_sel = 3'd1;
+    #5;
+
+    // SEL = 2
+    t_sel = 3'd2;
+    #5;
+
+    // SEL = 3
+    t_sel = 3'd3;
+    #5;
+
+    // SEL = 4
+    t_sel = 3'd4;
+    #5;
+
+    // SEL = 5
+    t_sel = 3'd5;
+    #5;
+
+    // SEL = 6
+    t_sel = 3'd6;
+    #5;
+
+    // SEL = 7
+    t_sel = 3'd7;
+    #5;
+
+    $finish;
   end
 
   initial
-    $monitor($time, " I0=%b I1=%b S=%b | Y=%b", t_i0, t_i1, t_s, t_y); // change as required
+    $monitor($time, " SEL=%b (%0d) | DOUT=%b (%0d)",
+             t_sel, t_sel, t_dout, t_dout);
 
 endmodule
